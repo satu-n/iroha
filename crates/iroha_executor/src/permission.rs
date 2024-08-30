58bc0a2bc1a63dd3adc017ada6bf237865ca64b5
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2024-08-31 06:25:21 +0900

    feat: predefine multisig world-level trigger in genesis
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- crates/iroha_executor/src/permission.rs
+++ crates/iroha_executor/src/permission.rs
@@ -117,6 +117,7 @@ declare_permissions! {
     iroha_executor_data_model::permission::parameter::{CanSetParameters},
     iroha_executor_data_model::permission::role::{CanManageRoles},
 
+    iroha_executor_data_model::permission::trigger::{CanRegisterAnyTrigger},
     iroha_executor_data_model::permission::trigger::{CanRegisterTrigger},
     iroha_executor_data_model::permission::trigger::{CanUnregisterTrigger},
     iroha_executor_data_model::permission::trigger::{CanModifyTrigger},
@@ -754,8 +755,8 @@ pub mod account {
 pub mod trigger {
     //! Module with pass conditions for trigger related tokens
     use iroha_executor_data_model::permission::trigger::{
-        CanExecuteTrigger, CanModifyTrigger, CanModifyTriggerMetadata, CanRegisterTrigger,
-        CanUnregisterTrigger,
+        CanExecuteTrigger, CanModifyTrigger, CanModifyTriggerMetadata, CanRegisterAnyTrigger,
+        CanRegisterTrigger, CanUnregisterTrigger,
     };
 
     use super::*;
@@ -816,6 +817,20 @@ pub mod trigger {
             Err(ValidationFail::NotPermitted(
                 "Can't give permission to access trigger owned by another account".to_owned(),
             ))
+        }
+    }
+
+    impl ValidateGrantRevoke for CanRegisterAnyTrigger {
+        fn validate_grant(&self, authority: &AccountId, context: &Context, host: &Iroha) -> Result {
+            OnlyGenesis::from(self).validate(authority, host, context)
+        }
+        fn validate_revoke(
+            &self,
+            authority: &AccountId,
+            context: &Context,
+            host: &Iroha,
+        ) -> Result {
+            OnlyGenesis::from(self).validate(authority, host, context)
         }
     }
 

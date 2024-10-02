7c7a680ac7d0b46ba1b37faace5b7a672af92506
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2024-10-02 18:07:52 +0900

    review fix: condition to rebuild multisig subordinate triggers
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- wasm_samples/multisig_domains/build.rs
+++ wasm_samples/multisig_domains/build.rs
@@ -5,7 +5,14 @@ use std::{io::Write, path::Path};
 const TRIGGER_DIR: &str = "../multisig_accounts";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo::rerun-if-changed={}", TRIGGER_DIR);
+    for path in [
+        TRIGGER_DIR,
+        "../../crates/iroha_data_model/src",
+        "../../crates/iroha_core/src/smartcontracts",
+        "../../crates/iroha_smart_contract/src",
+    ] {
+        println!("cargo::rerun-if-changed={path}");
+    }
 
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let wasm = iroha_wasm_builder::Builder::new(TRIGGER_DIR)

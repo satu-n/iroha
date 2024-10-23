4a8cef67afbd42058df8715e2ead0e46877f6dec
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2024-10-23 15:01:08 +0900

    wip add multisig data model into schema
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- crates/iroha_multisig_data_model/src/lib.rs
+++ crates/iroha_multisig_data_model/src/lib.rs
@@ -4,30 +4,34 @@
 
 extern crate alloc;
 
-use alloc::{collections::btree_map::BTreeMap, vec::Vec};
+use alloc::{collections::btree_map::BTreeMap, format, string::String, vec::Vec};
 
 use iroha_data_model::prelude::*;
+use iroha_schema::IntoSchema;
+use parity_scale_codec::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
 /// Arguments to register multisig account
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Clone, Decode, Encode, Serialize, Deserialize, IntoSchema)]
 pub struct MultisigAccountArgs {
     /// Multisig account to be registered
     /// WARNING: any corresponding private key allows the owner to manipulate this account as a ordinary personal account
     pub account: PublicKey,
     /// List of accounts and their relative weights of responsibility for the multisig
-    pub signatories: BTreeMap<AccountId, u8>,
+    pub signatories: BTreeMap<AccountId, Weight>,
     /// Threshold of total weight at which the multisig is considered authenticated
     pub quorum: u16,
     /// Multisig transaction time-to-live in milliseconds based on block timestamps. Defaults to [`DEFAULT_MULTISIG_TTL_MS`]
     pub transaction_ttl_ms: u64,
 }
 
+type Weight = u8;
+
 /// Default multisig transaction time-to-live in milliseconds based on block timestamps
 pub const DEFAULT_MULTISIG_TTL_MS: u64 = 60 * 60 * 1_000; // 1 hour
 
 /// Arguments to propose or approve multisig transaction
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Clone, Decode, Encode, Serialize, Deserialize, IntoSchema)]
 pub enum MultisigTransactionArgs {
     /// Propose instructions and initialize approvals with the proposer's one
     Propose(Vec<InstructionBox>),

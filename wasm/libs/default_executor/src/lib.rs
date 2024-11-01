5788893062cdb9b965c3d9e78ac427cd690dd120
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2024-11-01 20:41:16 +0900

    wip
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- wasm/libs/default_executor/src/lib.rs
+++ wasm/libs/default_executor/src/lib.rs
@@ -48,31 +48,6 @@ fn visit_custom(executor: &mut Executor, isi: &CustomInstruction) {
     deny!(executor, "Failed to parse custom instruction");
 }
 
-fn visit_multisig(executor: &mut Executor, isi: &MultisigInstructionBox) {
-    match isi {
-        MultisigInstructionBox::Register(isi) => visit_multisig_register(executor, isi),
-        MultisigInstructionBox::Propose(isi) => visit_multisig_propose(executor, isi),
-        MultisigInstructionBox:Approve(isi) => visit_multisig_approve(executor, isi),
-    }
-}
-
-fn visit_multisig_register(executor: &mut Executor, isi: &MultisigRegister) {
-            // Any account in domain can call multisig accounts registry to register any multisig account in the domain
-        // TODO Restrict access to the multisig signatories?
-        // TODO Impose proposal and approval process?
-    let registry = multisig_account_registry(isi.account().domain());
-
-
-    if isi.account().domain() == executor.context().authority.domain() {
-        execute!(executor, isi);
-    }
-
-    deny!(executor, "multisig account and its registrant must be in the same domain")
-}
-fn visit_multisig_propose(executor: &mut Executor, isi: &MultisigPropose) {}
-fn visit_multisig_approve(executor: &mut Executor, isi: &MultisigApprove) {}
-
-
 /// Migrate previous executor to the current version.
 /// Called by Iroha once just before upgrading executor.
 ///
---
+++ wasm/libs/default_executor/src/multisig/mod.rs
@@ -0,0 +1,24 @@
+mod account;
+mod transaction;
+
+fn visit_multisig(executor: &mut Executor, isi: &MultisigInstructionBox) {
+    match isi {
+        MultisigInstructionBox::Register(isi) => visit_multisig_register(executor, isi),
+        MultisigInstructionBox::Propose(isi) => visit_multisig_propose(executor, isi),
+        MultisigInstructionBox:Approve(isi) => visit_multisig_approve(executor, isi),
+    }
+}
+
+fn visit_multisig_register(executor: &mut Executor, isi: &MultisigRegister) {
+    // Any account in domain can call multisig accounts registry to register any multisig account in the domain
+    // TODO Restrict access to the multisig signatories?
+    // TODO Impose proposal and approval process?
+    if isi.account().domain() == executor.context().authority.domain() {
+        execute!(executor, isi);
+    }
+
+    deny!(executor, "multisig account and its registrant must be in the same domain")
+}
+fn visit_multisig_propose(executor: &mut Executor, isi: &MultisigPropose) {}
+
+fn visit_multisig_approve(executor: &mut Executor, isi: &MultisigApprove) {}
